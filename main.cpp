582b0d54e046eef3566e2723b4b25cbdeb63ac68
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-03-29 21:22:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   float a, b, x, y, z;
+    cout << "inserire quattro valori reali: a, b, x, y" << endl;
+    cin >> a >> b >> x >> y;
+
+    if ((x<0)&&(y>0)){
+        z= ((a*x) - (b*y));
+    } else if ((x>=0)&&(y<=0)){
+        z= ((a*(x*x)) - (b*y));
+    } else {
+        z= ((a*x) + (b*(y*y)));
+    }
+    cout << z;
    return 0;
 }
